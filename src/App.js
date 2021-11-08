3953347813eea2374455e30d7142646c0fcb441e
Jasmin Brunni <c0brja00@students.oamk.fi>
2021-11-08 19:13:01 +0200

    frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const URL = 'http://localhost/shoppinglist/index.php';
+
+
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [item, setItem] = useState('');
+  
+
+  function save(e){
+    e.preventDefault();
+    const json = JSON.stringify({description:item})
+    axios.post(URL + 'add.php', json,{
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then((response) => {
+      setItems(items => [...items,response.data]);
+      setItem('');
+    }).catch (error => {
+      alert(error.response.data.error)
+    });
+  }
+
+  useEffect(() => {
+    axios.get(URL)
+    .then((response) => {
+      setItems(response.data)
+    }).catch(error => {
+      alert(error.response ? error.response.data.error : error);
+    })
+  
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+    <h3>Shopping list</h3>
+    <form onSubmit={save}>
+      <label>New Item</label>
+      <input value={item} onChange={e => setItem(e.target.value)}/>
+      <button>Add</button>
+
+    </form>
+    <ol>
+      {items?.map(item => (
+        <li key={item.id}>{item.description}</li>
+      ))}
+    </ol>
     </div>
+
+    
   );
 }
+
 
 export default App;
